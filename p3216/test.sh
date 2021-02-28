3c58cd7236de21fa2ddd4ce31e850d4fc6da95ea
Eckhart Köppen <eck@40hz.org>
2021-02-28 19:45:36 +0200

    Add initial test file creation for p3216

---
+++ p3216/test.sh
@@ -0,0 +1,15 @@
+cat ../tests/setup.ft
+cat ../tests/tester.ft
+cat ../tests/prelimtest.ft
+cat ../tests/core.ft
+cat ../tests/math-sym.ft
+cat ../tests/dict-memory.ft
+cat ../tests/parsing.ft
+cat ../tests/defining.ft
+cat ../tests/compile.ft
+cat ../tests/pictured-out.ft
+cat ../tests/in-out.ft
+echo "Accept Test Input"
+cat ../tests/move.ft
+cat ../tests/dict-order.ft
+cat ../tests/final.ft
