4784715e01f274fa6e320fb70cf2ba9dc306e102
Eckhart Köppen <eck@40hz.org>
2025-04-19 09:18:51 +0300

    Build docs with Hugo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CoreForth-0
+
 Generic multi-target Forth system
+
+[Online Docummentation](https://ekoeppen.srht.site/coreforth-0)
---
+++ docs/mkmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd ..
+# dirs="avr common cortex cortex-stc drivers p3216 msp430"
+dirs="avr"
+find $dirs -type d | xargs -I % mkdir -p docs/content/%
+find $dirs -name '*.f[ts]' | \
+  xargs -I % sh -c "sed 's/^\([^\]\)/    \1/;s/\\\ //;s/\\\//;s/    include \(.*\)/[\1](.\/\1.md)\\n/' % >docs/content/%.md"
+popd
