73c2c2d2ac1b90adee2b9a664f3d9a3cdd951505
Eckhart Köppen <eck@40hz.org>
2025-04-19 13:28:36 +0300

    Add common words to documentation

--- docs/mkmd.sh
+++ docs/mkmd.sh
@@ -2,7 +2,7 @@
 
 pushd ..
 # dirs="avr common cortex cortex-stc drivers p3216 msp430"
-dirs="avr"
+dirs="avr common"
 find $dirs -type d | xargs -I % mkdir -p docs/content/%
 find $dirs -name '*.f[ts]' | \
   xargs -I % sh -c "sed 's/^\([^\]\)/    \1/;s/\\\ //;s/\\\//;s/    include \(.*\)/[\1](.\/\1.md)\\n/' % >docs/content/%.md"
