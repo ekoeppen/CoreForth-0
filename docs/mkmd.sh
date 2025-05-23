f8646604bf47d663b64d6631eda7e232bb00f8c0
Eckhart Köppen <eck@40hz.org>
2025-05-23 08:37:37 +0300

    Add more folders to documentation

--- docs/mkmd.sh
+++ docs/mkmd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pushd ..
-# dirs="avr common cortex cortex-stc drivers p3216 msp430"
-dirs="avr common"
+dirs="avr common cortex cortex-stc drivers p3216 msp430"
+# dirs="avr common"
 find $dirs -type d | xargs -I % mkdir -p docs/content/%
 find $dirs -name '*.f[ts]' | \
   xargs -I % sh -c "sed 's/^\([^\]\)/    \1/;s/\\\ //;s/\\\//;s/    include \(.*\)/[\1](.\/\1.md)\\n/' % >docs/content/%.md"
